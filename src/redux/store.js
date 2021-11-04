c29695b7c83c7519f674da4d41f859e92bd86ed7
Andrii Lypovetskyi <li_andr@ukr.net>
2021-11-04 12:17:11 +0200

    configureStore()

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import contactsReducer from './contacts/contacts-reduser';
+import { configureStore } from '@reduxjs/toolkit';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
 });
-
-const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
