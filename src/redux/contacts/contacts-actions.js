ed7856b0d75f3cd1f1ab1e8661f6cc185cdf96c6
Andrii Lypovetskyi <li_andr@ukr.net>
2021-11-04 12:18:17 +0200

    createAction()

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,29 +1,18 @@
 import actionTypes from './contacts-types';
+import { createAction } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
-const addContact = data => {
+const addContact = createAction(actionTypes.ADD, data => {
   return {
-    type: actionTypes.ADD,
     payload: {
       id: uuidv4(),
       name: data.name,
       number: data.number,
     },
   };
-};
-const deleteContact = dataId => {
-  return {
-    type: actionTypes.DELETE,
-    payload: dataId,
-  };
-};
-
-const filterContact = value => {
-  return {
-    type: actionTypes.FILTER,
-    payload: value,
-  };
-};
+});
+const deleteContact = createAction(actionTypes.DELETE);
+const filterContact = createAction(actionTypes.FILTER);
 
 const action = { addContact, deleteContact, filterContact };
 
